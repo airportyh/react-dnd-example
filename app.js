84285e0f4d798c8e666d1c6c70c4dc2eadca36d5
Toby Ho <airportyh@gmail.com>
2015-09-27 22:58:37 -0400

    Added DnD.

--- app.js
+++ app.js
@@ -13,17 +13,43 @@ class App extends React.Component {
     );
   }
 }
+App = ReactDnD.DragDropContext(ReactDnD.HTML5)(App);
 
 class TrashBin extends React.Component {
   render() {
-    return <div className="trash-bin"></div>;
+    return this.props.connectDropTarget(
+      <div className="trash-bin"></div>
+    );
   }
 }
+TrashBin = ReactDnD.DropTarget('fruit', {
+  drop() {
+    return { name: 'trash-bin' };
+  }
+}, (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget()
+}))(TrashBin);
 
 class Fruit extends React.Component {
   render() {
-    return <div className="fruit">{this.props.name}</div>;
+    return this.props.connectDragSource(
+      <div className="fruit">{this.props.name}</div>
+    );
   }
 }
+Fruit = ReactDnD.DragSource('fruit', {
+  beginDrag(props) {
+    console.log('begin drag', props.name);
+    return {
+      name: props.name
+    };
+  },
+  endDrag(props, monitor) {
+    let result = monitor.getDropResult();
+    console.log('dropped', props.name, 'onto', result);
+  }
+}, (connect, monitor) => ({
+  connectDragSource: connect.dragSource()
+}))(Fruit);
 
 React.render(<App/>, document.getElementById('content'));
